ef071fd188257ce63236bdb3453cacf4c56ee10b
Eden Mesfun <emesfun@gmail.com>
2018-01-23 15:41:35 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,8 @@
 def introduction(name)
+  puts "Hi, my name is #{name}."
 end
 
 def introduction_with_language(name, language)
   puts "Hi, my
- name is #{name} and I am learning to program in #{name}.\n"
+ name is #{name} and I am learning to program in #{name}."
 end #method
